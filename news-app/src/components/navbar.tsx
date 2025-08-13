b72b7e293632b70ca8d00e410a6478ccc64d7e42
ikana82 <ikanurasih82@gmail.com>
2025-08-13 22:23:54 +0700

    Add Server Success

--- news-app/src/components/navbar.tsx
+++ news-app/src/components/navbar.tsx
@@ -3,11 +3,14 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <nav>
-            <div className="flex gap-4 justify-center">
-                <Link href="/" className="bg-black text-white dark:bg-white dark:text-black px-4 py-2 rounded-md mt-3">Home</Link>
-                <Link href="/about" className="bg-black text-white dark:bg-white dark:text-black px-4 py-2 rounded-md mt-3">About</Link>
-                <Link href="/category/sport" className="bg-black text-white dark:bg-white dark:text-black px-4 py-2 rounded-md mt-3">Sport</Link>
-                <Link href="/category/tech" className="bg-black text-white dark:bg-white dark:text-black px-4 py-2 rounded-md mt-3">Tech</Link>
+            <div className="container flex gap-2 mx-auto justify-between py-4">
+                <h1 className="text-2xl font-bold">News</h1>
+            <div className="flex gap-2">
+                <Link href="/" className="px-4 py-2 rounded-md font-bold hover:bg-gray-100 dark:hover:bg-zinc-900">Home</Link>
+                <Link href="/about" className="px-4 py-2 rounded-md font-bold hover:bg-gray-100 dark:hover:bg-zinc-900">About</Link>
+                <Link href="/add-client" className="px-4 py-2 rounded-md font-bold hover:bg-gray-100 dark:hover:bg-zinc-900">Add Client</Link>
+                <Link href="/add-server" className="px-4 py-2 rounded-md font-bold hover:bg-gray-100 dark:hover:bg-zinc-900">Add Server</Link>
+            </div>
             </div>
         </nav>
     )
